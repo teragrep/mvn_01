9f5d79c210f8a3e23e65f0d9ae62728f7394e508
Strongest Number 9 <16169054+StrongestNumber9@users.noreply.github.com>
2024-09-05 10:49:35 +0300

    Enhances documentations a bit, fixes some syntax errors, adds example… (#18)
    * Enhances documentations a bit, fixes some syntax errors, adds example project
    
    * Adds more syntax insanity
    
    * Clarifications to docs, add how-to-exclude-files test
    
    * Adds a bit more of description
    
    * Remove too much indent on rat

---
+++ example-java-project/src/main/java/com/teragrep/ejp/Main.java
@@ -0,0 +1,14 @@
+/* License missing intentionally */
+package com.teragrep.ejp;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int[] numbers = new int[] { 67, 111, 110, 103, 114, 97, 116, 117,
+            108, 97, 116, 105, 111, 110, 115, 44, 32, 121, 111, 117, 32, 100, 105, 100, 32, 105,
+                    116, 33, 10
+    }; // What will the program print? ;)
+    for (int number : numbers) {         System.out.print(Character.toString(number));
+    }
+    }
+}
